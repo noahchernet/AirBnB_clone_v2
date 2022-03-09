f3b6ade15366c9d4d5450d2138a85bad14cf617d
Noah Chernet <noahchernet@yahoo.com>
2022-03-09 22:39:47 +0300

    Trying task 10, improved files for task 9

--- models/review.py
+++ models/review.py
@@ -8,8 +8,8 @@ from os import getenv
 
 class Review(BaseModel):
     """ Review classto store review information """
-    __tablename__ = ' reviews'
+    __tablename__ = 'reviews'
 
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
