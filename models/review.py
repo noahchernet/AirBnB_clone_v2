da776d4eb0c42d0585ce2f4f7023bfc496a20bf0
Noah Chernet <noahchernet@yahoo.com>
2022-03-09 22:43:40 +0300

    Fixed crashing in review.py

--- models/review.py
+++ models/review.py
@@ -6,7 +6,7 @@ from sqlalchemy import (
 from os import getenv
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = 'reviews'
 
