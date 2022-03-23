4d2f10f8aba2e32323ab619e0aed7b0ad14f72bf
Noah Chernet <noahchernet@yahoo.com>
2022-03-23 21:58:20 +0300

    Reverted changes made in for 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,21 +9,14 @@ env.hosts = ['18.232.153.3', '34.236.33.185']
 
 
 def do_deploy(archive_path):
-    '''Calls do_deploy_run and returns either True or
-    False if an exception is raised'''
-    try:
-        do_deploy_run(archive_path)
-    except Exception:
-        return False
-    return True
-
-
-def do_deploy_run(archive_path):
     '''Deploys archive to remote servers'''
 
     file_path = os.getcwd() + '/' + archive_path
     # Return false if the archive doesn't exist
-    os.path.getsize(os.getcwd() + '/' + archive_path)
+    try:
+        os.path.getsize(os.getcwd() + '/' + archive_path)
+    except Exception:
+        return False
 
     # Upload the archive to remote servers
     put(file_path, "/tmp/")
@@ -42,4 +35,3 @@ def do_deploy_run(archive_path):
     run('sudo rm -f -- /data/web_static/current')
     run('sudo ln -sf /data/web_static/releases/'
         + archive_path[9:-4] + ' /data/web_static/current')
-    return True
