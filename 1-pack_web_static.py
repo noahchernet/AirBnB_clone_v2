998ad1c2afc44574529cfca890215a2f25601b19
Noah Chernet <noahchernet@yahoo.com>
2022-03-23 17:20:46 +0300

    Done task #1, archives web_static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''Module 1-web_pack_static
+Packs the static web page folder web_static into a .tgz file
+'''
+from fabric.api import *
+from datetime import datetime
+import os
+
+
+def do_pack():
+    '''Compresses directory web_static into .tgz format'''
+    t = datetime.now()
+    file_name = "web_static_" + str(t.year) + str(t.month) + str(t.day) + str(t.hour) + str(t.minute) + str(t.second) + '.tgz'
+    local('mkdir -p versions')
+    local('tar -cvzf versions/{} web_static'.format(file_name))
+    try:
+        print('web_static packed: versions/' + file_name, "->", str(os.path.getsize(os.getcwd() + '/versions/' + file_name)) + "Bytes")
+    except Exception:
+        print('None')
