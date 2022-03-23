af0880199c6599c4edca97fd4fff722962161e39
Noah Chernet <noahchernet@yahoo.com>
2022-03-23 21:20:43 +0300

    Made do_deploy return a boolean value

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,14 +9,21 @@ env.hosts = ['18.232.153.3', '34.236.33.185']
 
 
 def do_deploy(archive_path):
+    '''Calls do_deploy_run and returns either True or
+    False if an exception is raised'''
+    try:
+        do_deploy_run(archive_path)
+    except Exception:
+        return False
+    return True
+
+
+def do_deploy_run(archive_path):
     '''Deploys archive to remote servers'''
 
     file_path = os.getcwd() + '/' + archive_path
     # Return false if the archive doesn't exist
-    try:
-        os.path.getsize(os.getcwd() + '/' + archive_path)
-    except Exception:
-        return False
+    os.path.getsize(os.getcwd() + '/' + archive_path)
 
     # Upload the archive to remote servers
     put(file_path, "/tmp/")
