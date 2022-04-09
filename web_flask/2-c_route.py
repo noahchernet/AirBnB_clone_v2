0e5e22ac2ee4e3c550693c6e590cb7456d957eb4
Noah Chernet <noahchernet@yahoo.com>
2022-04-09 15:38:51 +0300

    Done task 2, 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+'''Module 2-c_route
+A basic Flask app that writes "Hello, HBNB!" to the screen on route /
+and "HBNB" on route /hbnb
+and shows the text passed in the route c/<text>
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index_route():
+    '''Home page of the server'''
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def route_hbnb():
+    '''Prints "HBNB" only'''
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text_print(text):
+    '''Prints the text passed in the route'''
+    return "C {}\n".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
