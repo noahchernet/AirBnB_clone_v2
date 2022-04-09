38ae8f44182d1f0a14f6597304130ae0c7310ab9
Noah Chernet <noahchernet@yahoo.com>
2022-04-09 15:33:12 +0300

    Done task 0, 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''Module 0-hello_route
+A basic Flask app that writes "Hello, HBNB!" to the screen
+'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index_route():
+    '''Home page of the server'''
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
